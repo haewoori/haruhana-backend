e796c51569cc2be2dc76aff55394658eb30ef6ae
Kim <jwk@Kimui-MacBookAir.local>
2025-07-26 15:32:12 +0900

    INITIAL COMMIT

---
+++ src/main/java/hae/woori/onceaday/OnceadayApplication.java
@@ -0,0 +1,13 @@
+package hae.woori.onceaday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceadayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceadayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hae/woori/onceaday/OnceadayApplicationTests.java
@@ -0,0 +1,13 @@
+package hae.woori.onceaday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceadayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
