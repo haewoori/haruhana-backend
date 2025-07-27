b6aeab072f1c90737c9345b25cb9f24c38288b5e
김종원 Jongwon <tank3a@gmail.com>
2025-07-27 14:32:54 +0900

    docs: swagger 추가 (#24)
    - notification 내용 추가

--- src/main/java/hae/woori/onceaday/domain/notification/NotificationController.java
+++ src/main/java/hae/woori/onceaday/domain/notification/NotificationController.java
@@ -10,6 +10,7 @@ import hae.woori.onceaday.domain.notification.dto.NotificationAddDto;
 import hae.woori.onceaday.domain.notification.dto.NotificationGetDto;
 import hae.woori.onceaday.domain.notification.service.NotificationAddService;
 import hae.woori.onceaday.domain.notification.service.NotificationGetService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,15 +21,15 @@ public class NotificationController {
 	private final NotificationAddService notificationAddService;
 	private final NotificationGetService notificationGetService;
 
-	// 알림 추가 API
-	    @PostMapping("/add")
-	    public NotificationAddDto.Response addNotification(@RequestBody NotificationAddDto.Request request) {
-	        return notificationAddService.run(request);
-	    }
+	@Operation(description = "알림을 추가합니다. 가장 마지막에 추가된 알림이 보이고, 빈 문자열을 보내면 알림이 사라집니다")
+	@PostMapping("/add")
+	public NotificationAddDto.Response addNotification(@RequestBody NotificationAddDto.Request request) {
+		return notificationAddService.run(request);
+	}
 
-	    // 알림 전체 조회 API
-	    @GetMapping("/get")
-	    public NotificationGetDto.Response getNotifications() {
-	        return notificationGetService.run(new NotificationGetDto.Request());
-	    }
+	@Operation(description = "알림을 적을 메세지 제공. 빈 문자열이면 알림창을 지워야함")
+	@GetMapping("/get")
+	public NotificationGetDto.Response getNotifications() {
+		return notificationGetService.run(new NotificationGetDto.Request());
+	}
 }
