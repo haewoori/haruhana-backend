4826122a6a0441333d26dd5fa6128adaa41443fc
tank3a <tank3a@gmail.com>
2025-08-17 15:23:29 +0900

    feature: Study 조회/신청/취소 시 최대 참여자수 반영
    - category 추가, registered 삭제

--- src/main/java/hae/woori/onceaday/domain/study/mapper/StudyCardMapper.java
+++ src/main/java/hae/woori/onceaday/domain/study/mapper/StudyCardMapper.java
@@ -27,13 +27,6 @@ public interface StudyCardMapper {
         StudyUserProfileVo userProfile, @Context ParticipantResolver participantResolver);
 
     @AfterMapping
-    default void isRegistered(@MappingTarget StudyCardDto studyCardDto, StudyCardDocument studyCardDocument, @Context String userId, @Context StudyUserProfileVo userProfile) {
-        //TODO: 여기에 AND절로 신청내역을 보고 있는지도 확인해야함
-        studyCardDto.setRegistered(studyCardDocument.getUserId().equals(userId));
-        studyCardDto.setUserProfile(userProfile);
-    }
-
-    @AfterMapping
     default void fillParticipants(@MappingTarget StudyCardDto dto,
                                   StudyCardDocument doc,
                                   @Context String userId,
