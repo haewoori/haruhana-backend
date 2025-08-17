0d5c6f7cc820cd479bab1bafd9ed953b45733956
tank3a <tank3a@gmail.com>
2025-08-17 14:02:21 +0900

    feature: Study 조회/신청/취소 시 최대 참여자수 반영
    - swagger용 설명 추가

--- src/main/java/hae/woori/onceaday/domain/study/dto/StudyCardDto.java
+++ src/main/java/hae/woori/onceaday/domain/study/dto/StudyCardDto.java
@@ -5,11 +5,13 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import hae.woori.onceaday.domain.study.vo.StudyUserProfileVo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema(description = "스터디 카드의 상세 정보")
 public class StudyCardDto {
 
 	private String studyCardId;
@@ -21,10 +23,12 @@ public class StudyCardDto {
 	private LocalDateTime updatedTime;
 	private LocalDate dueDate;
 	private boolean online;
+	@Schema(description = "모집중/모집완료에 대한 구분. false면 모집완료")
 	private boolean available;
 	private boolean registered;
 	private boolean isMine;
 	List<String> participantIds;
+	@Schema(description = "참여자 이름 목록. ID 순서와 동일하게 유지됨")
 	List<String> participantNames;
 	boolean participated;
 
